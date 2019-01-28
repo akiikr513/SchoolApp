07053e77c69396f7e547e33415dbf786b370343d
akiik <akii.kr513@gmail.com>
2019-01-29 04:07:44 +0530

    2nd

--- app/src/main/java/com/example/akiik/schoolapp/AdminDash.java
+++ app/src/main/java/com/example/akiik/schoolapp/AdminDash.java
@@ -1,9 +1,12 @@
 package com.example.akiik.schoolapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AdminDash extends AppCompatActivity {
+public class AdminDash extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,41 @@ public class AdminDash extends AppCompatActivity {
 
         Bundle bundle=getIntent().getExtras();
 
+        Button but_stu=findViewById(R.id.stu);
+        Button but_teach=findViewById(R.id.teach);
+        Button but_par=findViewById(R.id.par);
+        Button but_acc=findViewById(R.id.acc);
+        Button but_lbr=findViewById(R.id.lib);
+
+        but_acc.setOnClickListener(this);
+        but_teach.setOnClickListener(this);
+        but_stu.setOnClickListener(this);
+        but_par.setOnClickListener(this);
+        but_lbr.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.teach:{
+                Intent iteach=new Intent(this,TeacherList.class);
+                startActivity(iteach);
+            }
+            break;
+            case R.id.acc:
+                break;
+            case R.id.stu:
+
+                break;
+            case R.id.par:
+                break;
+            case R.id.lib:
+                break;
+
+        }
+
+    }
+
+
 }
