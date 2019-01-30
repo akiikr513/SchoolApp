9bd07b0620763efd08787d0590c6ac646187afb6
akiik <akii.kr513@gmail.com>
2019-01-31 02:57:07 +0530

    2nd

--- app/src/main/java/com/example/akiik/schoolapp/AccountantFragment.java
+++ app/src/main/java/com/example/akiik/schoolapp/AccountantFragment.java
@@ -1,5 +1,6 @@
 package com.example.akiik.schoolapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AccountantFragment extends Fragment {
+    Button but;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_accountant,container,false);
+        View v = inflater.inflate(R.layout.fragment_accountant, container, false);
+
+        but = v.findViewById(R.id.but_acc_login);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent iaccdash = new Intent(getActivity(), AccountantDash.class);
+                startActivity(iaccdash);
+            }
+        });
+        return v;
     }
 }
